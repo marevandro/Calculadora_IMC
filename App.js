4ef9de924cb2b00c43e66b133bde4e6441c4eaf0
Márcio Evandro <marevandro95@gmail.com>
2022-05-04 14:15:56 -0300

    add do projeto

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState('');
+
+
+    function handSubmit(){
+        const alt = altura/100;
+        const imc = peso / (alt * alt);
+        
+        if(imc < 18.6){
+            alert('Você está abaixo do peso!' + imc.toFixed(2));
+        }else if (imc >= 18.6 && imc < 24.9){
+            alert('Peso ideal' + imc.toFixed(2));
+        }else if (imc >= 24.9 && imc < 34.9){
+            alert('Levemente acima do peso!' + imc.toFixed(2))
+        }
+    }
+ 
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>Calcule seu IMC</Text>
+            
+            <TextInput
+            style={styles.input}
+            value={peso}
+            onChangeText={ (peso) => setPeso(peso)} //quando eu digirtar algo ele vai salvar em uma variavel
+            placeholder="Peso (Kg)"
+            keyboardType='numeric'
+            />
+
+            <TextInput
+            style={styles.input}
+            value={altura}
+            onChangeText={ (altura) => setAltura(altura)} //quando eu digirtar algo ele vai salvar em uma variavel
+            placeholder="Altura (m)"
+            keyboardType='numeric'
+            />
+
+            <TouchableOpacity 
+            style={styles.button}
+            onPress={handSubmit}
+            >
+                    <Text style={styles.buttonText}>Calcular</Text>
+            </TouchableOpacity>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container:{ 
+        flex:1,
+    },
+    title:{
+        textAlign: 'center',
+        marginTop: 25,
+        fontSize: 30
+    },
+    input:{
+        backgroundColor: '#121212',
+        borderRadius: 10,
+        margin:15,
+        padding: 10,
+        color: '#FFF',
+        fontSize: 23,
+    },
+    button:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 13,
+        backgroundColor:'#6495ED',
+        padding: 10,
+    },
+    buttonText:{
+        color: '#FFF',
+        fontSize: 25,
+    }
 });
